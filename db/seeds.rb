6ab31866e4759e8b0cdbbd2927bce987bc186767
iannbraga <bragaprestes@gmail.com>
2024-05-20 23:26:43 -0300

    Atualizar seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,14 @@
 
 outros = [
   {
-    nome: "Ficha",
-    descricao: "de Sinuca",
+    nome: "Ficha de Sinuca",
     preco: 2.50,
     estoque: 0,
     categoria: "Outros",
     icone: "fa-solid fa-compact-disc",
   },
   {
-    nome: "Ficha",
-    descricao: "de Música",
+    nome: "Ficha de Música",
     preco: 1.00,
     estoque: 0,
     categoria: "Outros",
@@ -26,258 +24,258 @@ outros = [
 ]
 
 cervejas = [
-  # Antartica
   {
-    nome: "Antártica",
-    descricao: "269ml",
-    preco: 4.00,
+    nome: "Ficha de Sinuca",
+    preco: 2.5,
     estoque: 0,
-    categoria: "Cerveja",
-    icone: "fa-solid fa-beer-mug-empty",
+    categoria: "Outros",
   },
   {
-    nome: "Antártica",
-    descricao: "350ml",
-    preco: 5.00,
+    nome: "Ficha de Música",
+    preco: 1.0,
     estoque: 0,
-    categoria: "Cerveja",
-    icone: "fa-solid fa-beer-mug-empty",
+    categoria: "Outros",
   },
   {
-    nome: "Antártica",
-    descricao: "600ml",
-    preco: 10.00,
+    nome: "Antártica 269ml",
+    preco: 4.0,
     estoque: 0,
     categoria: "Cerveja",
     icone: "fa-solid fa-beer-mug-empty",
-  },
-  # Original
-  {
-    nome: "Original",
-    descricao: "269ml",
-    preco: 4.00,
-    estoque: 0,
-    categoria: "Cerveja",
-    icone: "fa-solid fa-beer-mug-empty",
+
   },
   {
-    nome: "Original",
-    descricao: "600ml",
-    preco: 12.00,
+    nome: "Antártica 350ml",
+    preco: 5.0,
     estoque: 0,
     categoria: "Cerveja",
     icone: "fa-solid fa-beer-mug-empty",
-  },
-  # Heineken
-  {
-    nome: "Heineken",
-    descricao: "269ml",
-    preco: 7.00,
-    estoque: 0,
-    categoria: "Cerveja",
-    icone: "fa-solid fa-beer-mug-empty",
+
   },
   {
-    nome: "Heineken",
-    descricao: "Long",
-    preco: 9.00,
+    nome: "Antártica 600ml",
+    preco: 10.0,
     estoque: 0,
     categoria: "Cerveja",
     icone: "fa-solid fa-beer-mug-empty",
+
   },
   {
-    nome: "Heineken",
-    descricao: "600ml",
-    preco: 15.00,
+    nome: "Original 269ml",
+    preco: 4.0,
     estoque: 0,
     categoria: "Cerveja",
     icone: "fa-solid fa-beer-mug-empty",
+
   },
   {
-    nome: "Heineken",
-    descricao: "Zero",
-    preco: 15.00,
+    nome: "Original 600ml",
+    preco: 12.0,
     estoque: 0,
     categoria: "Cerveja",
     icone: "fa-solid fa-beer-mug-empty",
-  },
-  # Amstel
-  {
-    nome: "Amstel",
-    descricao: "269ml",
-    preco: 14.00,
-    estoque: 0,
-    categoria: "Cerveja",
-    icone: "fa-solid fa-beer-mug-empty",
+
   },
   {
-    nome: "Amstel",
-    descricao: "600ml",
-    preco: 14.00,
+    nome: "Heineken 269ml",
+    preco: 7.0,
     estoque: 0,
     categoria: "Cerveja",
     icone: "fa-solid fa-beer-mug-empty",
-  },
-  # Budweiser
-  {
-    nome: "Budweiser",
-    descricao: "Long",
-    preco: 14.00,
-    estoque: 0,
-    categoria: "Cerveja",
-    icone: "fa-solid fa-beer-mug-empty",
+
   },
   {
-    nome: "Budweiser",
-    descricao: "269ml",
-    preco: 14.00,
+    nome: "Heineken Long",
+    preco: 10.0,
     estoque: 0,
     categoria: "Cerveja",
     icone: "fa-solid fa-beer-mug-empty",
+
   },
   {
-    nome: "Budweiser",
-    descricao: "600ml",
-    preco: 14.00,
+    nome: "Heineken 600ml",
+    preco: 15.0,
     estoque: 0,
     categoria: "Cerveja",
     icone: "fa-solid fa-beer-mug-empty",
-  },
-  # Corona
-  {
-    nome: "Corona",
-    descricao: "Latão",
-    preco: 14.00,
-    estoque: 0,
-    categoria: "Cerveja",
-    icone: "fa-solid fa-beer-mug-empty",
+
   },
   {
-    nome: "Corona",
-    descricao: "Long",
-    preco: 14.00,
+    nome: "Heineken Zero",
+    preco: 12.0,
     estoque: 0,
     categoria: "Cerveja",
     icone: "fa-solid fa-beer-mug-empty",
-  },
-  # Brahma
-  {
-    nome: "Brahama",
-    descricao: "600ml",
-    preco: 14.00,
-    estoque: 0,
-    categoria: "Cerveja",
-    icone: "fa-solid fa-beer-mug-empty",
+
   },
   {
-    nome: "Brahama",
-    descricao: "Zero",
-    preco: 10.00,
+    nome: "Amstel 269ml",
+    preco: 5.0,
     estoque: 0,
     categoria: "Cerveja",
     icone: "fa-solid fa-beer-mug-empty",
+
   },
-  # Kaiser
   {
-    nome: "Kaiser",
-    descricao: "350ml",
-    preco: 4.00,
+    nome: "Amstel 600ml",
+    preco: 14.0,
     estoque: 0,
     categoria: "Cerveja",
     icone: "fa-solid fa-beer-mug-empty",
+
   },
-  # Skol
   {
-    nome: "Skol",
-    descricao: "269ml",
-    preco: 5.00,
+    nome: "Budweiser Long",
+    preco: 10.0,
     estoque: 0,
     categoria: "Cerveja",
     icone: "fa-solid fa-beer-mug-empty",
+
+  },
+  {
+    nome: "Budweiser 269ml",
+    preco: 5.0,
+    estoque: 0,
+    categoria: "Cerveja",
+    icone: "fa-solid fa-beer-mug-empty",
+
+  },
+  {
+    nome: "Budweiser 600ml",
+    preco: 12.0,
+    estoque: 0,
+    categoria: "Cerveja",
+    icone: "fa-solid fa-beer-mug-empty",
+
+  },
+  {
+    nome: "Corona Latão",
+    preco: 7.0,
+    estoque: 0,
+    categoria: "Cerveja",
+    icone: "fa-solid fa-beer-mug-empty",
+
+  },
+  {
+    nome: "Corona Long",
+    preco: 10.0,
+    estoque: 0,
+    categoria: "Cerveja",
+    icone: "fa-solid fa-beer-mug-empty",
+
+  },
+  {
+    nome: "Brahama 600ml",
+    preco: 9.0,
+    estoque: 0,
+    categoria: "Cerveja",
+    icone: "fa-solid fa-beer-mug-empty",
+
+  },
+  {
+    nome: "Brahama Zero",
+    preco: 7.0,
+    estoque: 0,
+    categoria: "Cerveja",
+    icone: "fa-solid fa-beer-mug-empty",
+
+  },
+  {
+    nome: "Kaiser 350ml",
+    preco: 4.0,
+    estoque: 0,
+    categoria: "Cerveja",
+    icone: "fa-solid fa-beer-mug-empty",
+
+  },
+  {
+    nome: "Skol 269ml",
+    preco: 4.0,
+    estoque: 0,
+    categoria: "Cerveja",
+    icone: "fa-solid fa-beer-mug-empty",
+
   },
 ]
 
 bebidas = [
   {
-    nome: "Água",
-    descricao: "s/ gás",
-    preco: 4,
+    nome: "Água c/ gás",
+    preco: 0,
     estoque: 0,
     categoria: "Bebidas",
     icone: "fa-solid fa-bottle-water",
   },
   {
-    nome: "Água",
-    descricao: "c/ gás",
-    preco: 5,
+    nome: "Água s/ gás",
+    preco: 0,
     estoque: 0,
     categoria: "Bebidas",
     icone: "fa-solid fa-bottle-water",
   },
   # Coca Cola
   {
-    nome: "Coca",
-    descricao: "350ml",
-    preco: 5,
+    nome: "Coca 350ml",
+    preco: 0,
     estoque: 0,
     categoria: "Bebidas",
     icone: "fa-solid fa-water",
   },
   {
-    nome: "Coca",
-    descricao: "KS",
-    preco: 5,
+    nome: "Coca KS",
+    preco: 0,
     estoque: 0,
     categoria: "Bebidas",
     icone: "fa-solid fa-water",
   },
   {
-    nome: "Coca",
-    descricao: "600ml",
-    preco: 7,
+    nome: "Coca 600ml",
+    preco: 0,
     estoque: 0,
     categoria: "Bebidas",
     icone: "fa-solid fa-water",
   },
   {
-    nome: "Coca",
-    descricao: "1L",
-    preco: 10,
+    nome: "Coca 1L",
+    preco: 0,
     estoque: 0,
     categoria: "Bebidas",
     icone: "fa-solid fa-water",
   },
   {
-    nome: "Coca",
-    descricao: "2L",
-    preco: 15,
+    nome: "Coca 2L",
+    preco: 0,
     estoque: 0,
     categoria: "Bebidas",
     icone: "fa-solid fa-water",
   },
   # Guaraná
   {
-    nome: "Guaraná",
-    descricao: "2L",
-    preco: 10,
+    nome: "Guaraná 1L",
+    preco: 0,
+    estoque: 0,
+    categoria: "Bebidas",
+    icone: "fa-solid fa-water",
+  },
+  {
+    nome: "Guaraná 2L",
+    preco: 0,
     estoque: 0,
     categoria: "Bebidas",
     icone: "fa-solid fa-water",
   },
   # Sukita
   {
-    nome: "Sukita",
-    descricao: "600ml",
-    preco: 5,
+    nome: "Sukita 600ml",
+    preco: 0,
     estoque: 0,
     categoria: "Bebidas",
     icone: "fa-solid fa-water",
   },
   # Tônica
   {
-    nome: "Tônica",
-    descricao: "Lata",
-    preco: 6,
+    nome: "Tônica Lata",
+    preco: 0,
     estoque: 0,
     categoria: "Bebidas",
     icone: "fa-solid fa-water",
@@ -285,35 +283,31 @@ bebidas = [
   # Gatorede
   {
     nome: "Gatorede",
-    descricao: "",
-    preco: 8,
+    preco: 0,
     estoque: 0,
     categoria: "Bebidas",
     icone: "fa-solid fa-water",
   },
   # H2O
   {
-    nome: "H2O",
-    descricao: "",
-    preco: 6,
+    nome: "H2O Lata",
+    preco: 0,
     estoque: 0,
     categoria: "Bebidas",
     icone: "fa-solid fa-water",
   },
   # Suco
   {
-    nome: "Suco",
-    descricao: "1L",
-    preco: 8,
+    nome: "Suco 1L",
+    preco: 0,
     estoque: 0,
     categoria: "Bebidas",
     icone: "fa-solid fa-water",
   },
   # Água de coco
   {
-    nome: "Água",
-    descricao: "de Coco",
-    preco: 12,
+    nome: "Água de Coco",
+    preco: 0,
     estoque: 0,
     categoria: "Bebidas",
     icone: "fa-solid fa-water",
@@ -321,24 +315,21 @@ bebidas = [
   # Energéticos
   {
     nome: "Monster",
-    descricao: "",
-    preco: 12,
+    preco: 0,
     estoque: 0,
     categoria: "Bebidas",
     icone: "fa-solid fa-water",
   },
   {
     nome: "Redbull",
-    descricao: "",
-    preco: 12,
+    preco: 0,
     estoque: 0,
     categoria: "Bebidas",
     icone: "fa-solid fa-water",
   },
   {
     nome: "FireNigth",
-    descricao: "de Coco",
-    preco: 10,
+    preco: 0,
     estoque: 0,
     categoria: "Bebidas",
     icone: "fa-solid fa-water",
@@ -349,7 +340,6 @@ doses = [
   # Whisky
   {
     nome: "Dreher",
-    descricao: "",
     preco: 4,
     estoque: 0,
     categoria: "Dose",
@@ -357,47 +347,41 @@ doses = [
   },
   {
     nome: "Ballantinos",
-    descricao: "",
-    preco: 10,
+    preco: 4,
     estoque: 0,
     categoria: "Dose",
     icone: "fa-solid fa-whiskey-glass",
   },
   {
     nome: "Passaporte",
-    descricao: "",
-    preco: 10,
+    preco: 4,
     estoque: 0,
     categoria: "Dose",
     icone: "fa-solid fa-whiskey-glass",
   },
   {
     nome: "Montila",
-    descricao: "",
-    preco: 5,
+    preco: 4,
     estoque: 0,
     categoria: "Dose",
     icone: "fa-solid fa-whiskey-glass",
   },
   {
     nome: "Jurupinga",
-    descricao: "",
-    preco: 5,
+    preco: 4,
     estoque: 0,
     categoria: "Dose",
     icone: "fa-solid fa-whiskey-glass",
   },
   {
     nome: "José Cuervo",
-    descricao: "",
-    preco: 10,
+    preco: 4,
     estoque: 0,
     categoria: "Dose",
     icone: "fa-solid fa-whiskey-glass",
   },
   {
     nome: "Paratudo",
-    descricao: "",
     preco: 4,
     estoque: 0,
     categoria: "Dose",
@@ -405,7 +389,6 @@ doses = [
   },
   {
     nome: "Domus",
-    descricao: "",
     preco: 4,
     estoque: 0,
     categoria: "Dose",
@@ -413,32 +396,28 @@ doses = [
   },
   # Cachaças
   {
-    nome: "Cachaça",
-    descricao: "Murici",
+    nome: "Cachaça de Murici",
     preco: 4,
     estoque: 0,
     categoria: "Dose",
     icone: "fa-solid fa-whiskey-glass",
   },
   {
-    nome: "Cachaça",
-    descricao: "Abacaxi",
+    nome: "Cachaça de Abacaxi",
     preco: 4,
     estoque: 0,
     categoria: "Dose",
     icone: "fa-solid fa-whiskey-glass",
   },
   {
-    nome: "Cachaça",
-    descricao: "Caja",
+    nome: "Cachaça de Caja",
     preco: 4,
     estoque: 0,
     categoria: "Dose",
     icone: "fa-solid fa-whiskey-glass",
   },
   {
-    nome: "Cachaça",
-    descricao: "Pimenta biquinho",
+    nome: "Cachaça de Pimenta biquinho",
     preco: 4,
     estoque: 0,
     categoria: "Dose",
@@ -450,7 +429,6 @@ doses = [
 salgados = [
   {
     nome: "Torcida",
-    descricao: "",
     preco: 7.00,
     estoque: 0,
     categoria: "Salgados",
@@ -458,7 +436,6 @@ salgados = [
   },
   {
     nome: "Micos",
-    descricao: "",
     preco: 3.00,
     estoque: 0,
     categoria: "Salgados",
@@ -466,7 +443,6 @@ salgados = [
   },
   {
     nome: "Micão",
-    descricao: "",
     preco: 4.00,
     estoque: 0,
     categoria: "Salgados",
@@ -474,7 +450,6 @@ salgados = [
   },
   {
     nome: "Torresmo",
-    descricao: "",
     preco: 7.00,
     estoque: 0,
     categoria: "Salgados",
@@ -484,48 +459,49 @@ salgados = [
 
 porcoes = [
   {
-    nome: "Batata simples",
-    descricao: "",
+    nome: "Fritas simples",
     preco: 28,
     estoque: 0,
     categoria: "Porções",
     icone: "fa-solid fa-utensils",
   },
   {
-    nome: "Batata com calabresa",
-    descricao: "",
+    nome: "Fritas c/ calabresa",
     preco: 35,
     estoque: 0,
     categoria: "Porções",
     icone: "fa-solid fa-utensils",
   },
   {
-    nome: "Batata com calabresa e cheddar",
-    descricao: "",
+    nome: "Fritas c/ calabresa e cheddar",
     preco: 40,
     estoque: 0,
     categoria: "Porções",
     icone: "fa-solid fa-utensils",
   },
   {
-    nome: "Frango a passarinho",
-    descricao: "",
+    nome: "Frango à passarinho",
     preco: 38,
     estoque: 0,
     categoria: "Porções",
     icone: "fa-solid fa-utensils",
   },
   {
-    nome: "Frango a passarinho c/ calabresa",
-    descricao: "",
-    preco: 42,
+    nome: "Frango à passarinho c/ fritas",
+    preco: 40,
     estoque: 0,
     categoria: "Porções",
     icone: "fa-solid fa-utensils",
   },
   {
-    nome: "Frango a passarinho c/ fritas, calabresa e cheddar",
-    descricao: "",
+    nome: "Frango à passarinho c/ fritas e calabresa",
+    preco: 55,
+    estoque: 0,
+    categoria: "Porções",
+    icone: "fa-solid fa-utensils",
+  },
+  {
+    nome: "Frango à passarinho c/ fritas, calabresa e cheddar",
     preco: 55,
     estoque: 0,
     categoria: "Porções",
@@ -533,7 +509,6 @@ porcoes = [
   },
   {
     nome: "Calabresa acebolada",
-    descricao: "",
     preco: 20,
     estoque: 0,
     categoria: "Porções",
@@ -541,34 +516,34 @@ porcoes = [
   },
 ]
 
-outros.each do |outro|
-  p "Cadastrando #{outro[:nome]}"
-  Produto.create(outro)
+outros.each do |item|
+  p "Cadastrando #{item[:nome]}"
+  Produto.create(item)
 end
 
-cervejas.each do |cerveja|
-  p "Cadastrando #{cerveja[:nome]}"
-  Produto.create(cerveja)
+cervejas.each do |item|
+  p "Cadastrando #{item[:nome]}"
+  Produto.create(item)
 end
 
-bebidas.each do |bebida|
-  p "Cadastrando #{bebida[:nome]}"
-  Produto.create(bebida)
+bebidas.each do |item|
+  p "Cadastrando #{item[:nome]}"
+  Produto.create(item)
 end
 
-doses.each do |dose|
-  p "Cadastrando #{dose[:nome]}"
-  Produto.create(dose)
+doses.each do |item|
+  p "Cadastrando #{item[:nome]}"
+  Produto.create(item)
 end
 
-salgados.each do |salgado|
-  p "Cadastrando #{salgado[:nome]}"
-  Produto.create(salgado)
+salgados.each do |item|
+  p "Cadastrando #{item[:nome]}"
+  Produto.create(item)
 end
 
-porcoes.each do |porcao|
-  p "Cadastrando #{porcao[:nome]}"
-  Produto.create(porcao)
+porcoes.each do |item|
+  p "Cadastrando #{item[:nome]}"
+  Produto.create(item)
 end
 
 (1..20).each do |i|
