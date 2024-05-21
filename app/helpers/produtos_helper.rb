fa2953ef4a4855453af2dc33e9a320eda8af77b1
iannbraga <bragaprestes@gmail.com>
2024-05-21 00:37:01 -0300

    Helper de icones para o formulario

--- app/helpers/produtos_helper.rb
+++ app/helpers/produtos_helper.rb
@@ -1,2 +1,14 @@
 module ProdutosHelper
+  def mostrar_icones
+    icones = [
+      "fa-solid fa-compact-disc",
+      "fa-solid fa-music",
+      "fa-solid fa-beer-mug-empty",
+      "fa-solid fa-bottle-water",
+      "fa-solid fa-water",
+      "fa-solid fa-whiskey-glass",
+      "fa-solid fa-utensils",
+    ]
+    icones.map { |icone| ["#{icone}"] }
+  end
 end
